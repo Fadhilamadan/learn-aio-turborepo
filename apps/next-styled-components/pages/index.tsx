a8a9de724cacecea26db75e43eef81b32cde863e
Fadhil Amadan <git@fadhilamadan.com>
2023-02-21 11:00:57 +0700

    refactor(app): add wrapper and title on next styled components

--- apps/next-styled-components/pages/index.tsx
+++ apps/next-styled-components/pages/index.tsx
@@ -1,5 +1,16 @@
 import styled, { css } from 'styled-components';
 
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: palevioletred;
+`;
+
 const Button = styled.button`
   background: transparent;
   border-radius: 3px;
@@ -18,10 +29,12 @@ const Button = styled.button`
 
 const Home = () => {
   return (
-    <>
+    <Wrapper>
+      <Title>Hello World!</Title>
+
       <Button primary>Primary</Button>
       <Button>Second</Button>
-    </>
+    </Wrapper>
   );
 };
 
