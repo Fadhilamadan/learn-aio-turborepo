1b4c7ae991513b70354b0bedc36e92cc53e77dcd
Fadhil Amadan <git@fadhilamadan.com>
2023-02-28 13:23:21 +0700

    refactor(app): improve international route

--- apps/next-i18n/pages/index.tsx
+++ apps/next-i18n/pages/index.tsx
@@ -1,9 +1,33 @@
 import { useTranslation } from 'react-i18next';
+import { useSyncLanguage } from 'ni18n';
+
+import { useGlobalContext } from '@/contexts';
 
 const Home = () => {
   const { t } = useTranslation();
+  const { locals, setLocals } = useGlobalContext();
 
-  return <>{t('hello')}</>;
+  useSyncLanguage(locals);
+
+  const handleLocales = (e: any) => {
+    setLocals(e.target.value);
+  };
+
+  return (
+    <>
+      <input type="radio" name="locales" value="id" onChange={handleLocales} />
+      <label>Indonesia</label>
+      <br />
+
+      <input type="radio" name="locales" value="en" onChange={handleLocales} />
+      <label>English</label>
+      <br />
+
+      <br />
+
+      {t('hello')}
+    </>
+  );
 };
 
 export default Home;
